1dd89981bd1f237130d4edfebb1625420fe34acc
hexaredecimal <mfanakagama@gmail.com>
2025-02-19 16:00:49 +0200

    compile.sh: Only compile the system

--- compile.sh
+++ compile.sh
@@ -3,16 +3,4 @@
 set -e
 # Compile the OberonX System
 ant
-
-cd ./built-ins/
-
-find . -maxdepth 1 -type d ! -name "." | while read dir; do
-  echo "Compling: $dir"
-  cd $dir
-  # Compile the app
-  ant
-  cp -r dist/*.jar dist/lib ../../apps
-  cd ..
-done
-
-cd ..
+./compile_apps.sh
