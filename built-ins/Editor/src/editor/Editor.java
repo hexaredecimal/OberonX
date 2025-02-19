ecb7a99372c4fe269203a8b4f9b73ed99ff2b90a
hexaredecimal <mfanakagama@gmail.com>
2025-02-19 17:39:33 +0200

    Editor: Ported the Editor to its own app

---
+++ built-ins/Editor/src/editor/Editor.java
@@ -0,0 +1,37 @@
+package editor;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+
+import frames.Frame;
+import frames.FrameFactory;
+import oberonui.Window;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+public class Editor extends Window {
+	public Editor() {
+		super(FrameFactory.getFrame(Frame.Text | Frame.Editable), "Edit.app");
+	}
+
+	@Override
+	public void handleCommand(String command, String[] args) {
+		System.out.println("Editor: " + command);
+		super.handleCommand(command, args);
+	}
+
+	@Override
+	public void processArgs(String ... args) {
+		super.getFrame().processArgs(args);
+	}
+	
+	@Override
+	public String[] getCommands() {
+		return new String[]{"Editor.write", "Editor.open"};
+	}
+}
+
