706f3693984d6b0407c5352f6138d5f8e47d5b19
hexaredecimal <mfanakagama@gmail.com>
2025-02-21 18:06:06 +0200

    Frame: removed the webview and adjusted the flag values

--- src/frames/Frame.java
+++ src/frames/Frame.java
@@ -9,12 +9,11 @@ import java.awt.Component;
 
 
 public abstract class Frame {
-	public static int Text = 1 << 0;
-	public static int Canvas = 1 << 2;
-	public static int WebView = 1 << 3;
-	public static int Editable = 1 << 4;
-	public static int Basic = 1 << 5;
-	public static int Indent = 1 << 6;
+	public static int Text = 1;
+	public static int Canvas = 1 << 1;
+	public static int Editable = 1 << 2;
+	public static int Basic = 1 << 3;
+	public static int Indent = 1 << 4;
 	
 	private Component parent;
 
